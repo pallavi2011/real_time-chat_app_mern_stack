790ba4030417212d79cb7ceda1a6c6cfea397b82
Pallavi <pallavijorapur20@gmail.com>
2024-01-06 12:12:25 -0700

    initial setup for frontend and backend

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express()
+const dotenv = require('dotenv')
+dotenv.config()
+
+const PORT = process.env.PORT || 5000
+
+app.get('/', (req, res) => res.send('API is running'))
+
+app.get('/api/chat', (req, res) => {
+    res.send("chats")
+})
+
+app.get('/api/chat/:id', (req, res) => {
+    res.send("chats")
+})
+app.listen(PORT, console.log(`PORT is running on ${PORT}`))
